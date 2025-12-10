ecd74da6fa2bce09c5a8c404bb4c95dffc4de0aa
Israel Ben Zeev <ibz0502291330@gmail.com>
2025-12-10 17:30:26 +0200

    1

--- test2.py
+++ test2.py
@@ -9,13 +9,19 @@ def do_action(user):
 
 # פונקציה שבודקת את כל היוזרים
 def check_users():
-    result = supabase.auth.admin.list_users()
-    print("result: ", result)
-    # send_message_to_telegram(chat_id=result[0]["user_id"], text="TEST TEST TEST...")
-    # for user in users.data:
-        # כאן אפשר לבדוק לפי שעה אם רוצים
-        # if user["time"] == now:
-        # do_action(user)
+    users = supabase.auth.admin.list_users()
+
+    for user in users:
+        print("ID:", user.id)
+        print("Email:", user.email)
+        print("Name:", user.user_metadata.get("full_name"))
+        print("Last login:", user.last_sign_in_at)
+        print("--------------------------------")
+        # send_message_to_telegram(chat_id=result[0]["user_id"], text="TEST TEST TEST...")
+        # for user in users.data:
+            # כאן אפשר לבדוק לפי שעה אם רוצים
+            # if user["time"] == now:
+            # do_action(user)
 
 # קובע שיבוצע כל 3 שניות (לבדיקה)
 schedule.every(2).seconds.do(check_users)
@@ -23,7 +29,6 @@ schedule.every(2).seconds.do(check_users)
 # לולאה שמריצה את ה-schedule
 try:
     while True:
-        print("-----------------------------------------")
         schedule.run_pending()
         time.sleep(1)
 except KeyboardInterrupt:
