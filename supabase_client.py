c2dbf19c231a226d82548834ae89518f9c5c03bf
IsraelBenZeev <ibz0502291330@gmail.com>
2026-02-27 14:17:26 +0200

    fix

--- supabase_client.py
+++ supabase_client.py
@@ -5,6 +5,6 @@ from dotenv import load_dotenv
 load_dotenv(override=True)
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
+SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_ROLE_KEY")
 
 supabase = create_client(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
